542b063bab53fbf5b80b05be190df08238dae7d2
joe-herbert <joe@joeherbert.dev>
2020-03-30 17:26:09 +0100

    system set up

--- customHTMLElements/accordion/accordion.ts
+++ customHTMLElements/accordion/accordion.ts
@@ -1,10 +1,10 @@
 import {addLoadEvent} from "../../ts/global";
 addLoadEvent(() => {
-    var acc = document.getElementsByClassName("accordion");
+    let acc = document.getElementsByClassName("accordion");
     for (var i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
             this.classList.toggle("active");
-            var panel = this.nextElementSibling;
+            let panel = this.nextElementSibling;
             if (panel.style.maxHeight) {
                 panel.style.maxHeight = null;
             } else {
