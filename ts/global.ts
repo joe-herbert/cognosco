fd4cd7d2e39812c5f495dc7dafec0843a74f98a1
joe-herbert <joe@joeherbert.dev>
2020-04-05 03:08:19 +0100

    topics page

--- ts/global.ts
+++ ts/global.ts
@@ -1,4 +1,57 @@
-var topics = {
+//reusable useful functions
+function hasClass(el:HTMLElement, className:string) {
+    return el.classList ? el.classList.contains(className) : new RegExp('\\b' + className + '\\b').test(el.className);
+}
+
+function addClass(el: HTMLElement, className: string) {
+    if (el.classList) el.classList.add(className);
+    else if (!hasClass(el, className)) el.className += ' ' + className;
+}
+
+function removeClass(el: HTMLElement, className: string) {
+    if (el.classList) el.classList.remove(className);
+    else el.className = el.className.replace(new RegExp('\\b' + className + '\\b', 'g'), '');
+}
+
+function toggleClass(el: HTMLElement, className: string) {
+    if (hasClass(el, className)) {
+        removeClass(el, className);
+    } else {
+        addClass(el, className);
+    }
+}
+
+function escapeRegExp(string:string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+
+function capitaliseFirst(string:string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function getNextSibling(elem:HTMLElement, selector:string) {
+    var sibling = elem.nextElementSibling;
+
+    if (!selector) return sibling;
+
+    while (sibling) {
+        if (sibling.matches(selector)) return sibling;
+        sibling = sibling.nextElementSibling;
+    }
+};
+
+function getPreviousSibling(elem:HTMLElement, selector:string) {
+    var sibling = elem.previousElementSibling;
+
+    if (!selector) return sibling;
+
+    while (sibling) {
+        if (sibling.matches(selector)) return sibling;
+        sibling = sibling.previousElementSibling;
+    }
+};
+
+let topicsObject: object = {
     "DIY": {
         "def": "Plumbing, plastering, fixings, anything you'd do when you're building something.",
         "topics": {
@@ -12,7 +65,7 @@ var topics = {
             "Under the bonnet": {
                 "def": "The maze of pipes and things under the bonnet are all described here, from engines to washer fluid.",
                 "topics": {
-                    "engine": true
+                    "Engine": true
                 }
             },
             "Driving": true
